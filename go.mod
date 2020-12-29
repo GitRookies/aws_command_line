9501da1e0ee62559f39cf510d57351268ffb8028
Dave Augustus <davetweetlive@gmail.com>
2020-12-30 00:58:09 +0530

    ec2 monitor implementation

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/davetweetlive/aws_command_line
 go 1.15
 
 require (
-	github.com/aws/aws-sdk-go v1.36.15 // indirect
+	github.com/aws/aws-sdk-go v1.36.16
 	gopkg.in/yaml.v2 v2.4.0
 )
--- main.go
+++ main.go
@@ -2,13 +2,65 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/davetweetlive/aws_command_line/conf"
 )
 
 func main() {
+	// Establishing session
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String("provide region"),
+		Credentials: credentials.NewStaticCredentials(conf.AccessTokenEC2, conf.AccessSecretEC2, ""),
+	},
+	)
 
-	fmt.Println(conf.AccessTokenEC2)
-	fmt.Println(conf.AccessSecretEC2)
+	if err != nil {
+		// TODO: Handle error
+		fmt.Println(sess)
+	}
 
+	// On/Off
+	svc := ec2.New(sess)
+	action := os.Args[1]
+	action = strings.ToLower(action)
+	if action == "on" {
+		input := &ec2.MonitorInstancesInput{
+			InstanceIds: []*string{
+				aws.String("i-026901d8bb8c8d9d0"),
+			},
+			DryRun: aws.Bool(true),
+		}
+		result, err := svc.MonitorInstances(input)
+		awsErr, ok := err.(awserr.Error)
+		fmt.Println("Some changes")
+		fmt.Println(result)
+		fmt.Println(awsErr)
+		fmt.Println(ok)
+
+		if ok && awsErr.Code() == "DryRunOperation" {
+			input.DryRun = aws.Bool(false)
+			result, err = svc.MonitorInstances(input)
+			if err != nil {
+				fmt.Println("Error", err)
+			} else {
+				fmt.Println("Success", result.InstanceMonitorings)
+			}
+		} else {
+			fmt.Println("Error", err)
+		}
+
+	} else if action == "off" {
+		// Turn the instance off
+	} else if action == "restart" {
+		// Restart the server
+	} else {
+		fmt.Println("No action performed")
+	}
 }
