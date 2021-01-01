7f0739a79ca2a3ed757c1f1bb11c3e59d9e54d46
Dave Augustus <davetweetlive@gmail.com>
2021-01-01 22:26:24 +0530

    Issue 1 addressed

--- ec2/ec2.go
+++ ec2/ec2.go
@@ -1,1 +1,62 @@
 package ec2
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/ec2"
+	"github.com/davetweetlive/aws_command_line/conf"
+)
+
+func EC2Workflow() {
+	// Establishing session
+	sess, err := session.NewSession(&aws.Config{
+		Region:      aws.String(conf.Region),
+		Credentials: credentials.NewStaticCredentials(conf.AccessTokenEC2, conf.AccessSecretEC2, ""),
+	},
+	)
+
+	if err != nil {
+		// TODO: Handle error
+
+	}
+
+	// On/Off
+	svc := ec2.New(sess)
+	action := os.Args[2]
+	action = strings.ToLower(action)
+	if action == "on" {
+		input := &ec2.MonitorInstancesInput{
+			InstanceIds: []*string{
+				aws.String(conf.InstanceId),
+			},
+			DryRun: aws.Bool(true),
+		}
+		result, err := svc.MonitorInstances(input)
+		awsErr, ok := err.(awserr.Error)
+
+		if ok && awsErr.Code() == "DryRunOperation" {
+			input.DryRun = aws.Bool(false)
+			result, err = svc.MonitorInstances(input)
+			if err != nil {
+				fmt.Println("Error", err)
+			} else {
+				fmt.Println("Success", result.InstanceMonitorings)
+			}
+		} else {
+			fmt.Println("Error", err)
+		}
+
+	} else if action == "off" {
+		// Turn the instance off
+	} else if action == "restart" {
+		// Restart the server
+	} else {
+		fmt.Println("No action performed")
+	}
+}
--- main.go
+++ main.go
@@ -5,62 +5,20 @@ import (
 	"os"
 	"strings"
 
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/ec2"
-	"github.com/davetweetlive/aws_command_line/conf"
+	"github.com/davetweetlive/aws_command_line/ec2"
 )
 
 func main() {
-	// Establishing session
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String("provide region"),
-		Credentials: credentials.NewStaticCredentials(conf.AccessTokenEC2, conf.AccessSecretEC2, ""),
-	},
-	)
+	choices := os.Args[1:]
+	fmt.Println(choices[0])
 
-	if err != nil {
-		// TODO: Handle error
-		fmt.Println(sess)
+	if len(choices) == 0 {
+		fmt.Println(strings.Repeat("#", 40))
+		fmt.Println("Make sure you peovide the service name as the first parameter!")
+		fmt.Println("Eg. 'aws ec2 start'")
+		fmt.Println(strings.Repeat("#", 40))
 	}
-
-	// On/Off
-	svc := ec2.New(sess)
-	action := os.Args[1]
-	action = strings.ToLower(action)
-	if action == "on" {
-		input := &ec2.MonitorInstancesInput{
-			InstanceIds: []*string{
-				aws.String("i-026901d8bb8c8d9d0"),
-			},
-			DryRun: aws.Bool(true),
-		}
-		result, err := svc.MonitorInstances(input)
-		awsErr, ok := err.(awserr.Error)
-		fmt.Println("Some changes")
-		fmt.Println(result)
-		fmt.Println(awsErr)
-		fmt.Println(ok)
-
-		if ok && awsErr.Code() == "DryRunOperation" {
-			input.DryRun = aws.Bool(false)
-			result, err = svc.MonitorInstances(input)
-			if err != nil {
-				fmt.Println("Error", err)
-			} else {
-				fmt.Println("Success", result.InstanceMonitorings)
-			}
-		} else {
-			fmt.Println("Error", err)
-		}
-
-	} else if action == "off" {
-		// Turn the instance off
-	} else if action == "restart" {
-		// Restart the server
-	} else {
-		fmt.Println("No action performed")
+	if choices[0] == "ec2" {
+		ec2.EC2Workflow()
 	}
 }
